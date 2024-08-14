803f977d68069190c7d0be80e9661b12843c869c
Antonio Muto <93258305+AntonioMuto@users.noreply.github.com>
2024-08-14 15:06:01 +0200

    remove log and error

--- services/liveService.js
+++ services/liveService.js
@@ -9,41 +9,51 @@ const INFO = process.env.INFO;
 const arrayLeagues = [55,86,47,53,54,87,57,61]
 
 var liveMatches = [];
-
 var matchesDetails = new Map();
 
 const uploadLiveMatches = async () => {
     try {
         const formattedDate = getFormattedDate();
         const response = await axios.get(`${API_URL}matches?date=${formattedDate}&timezone=Europe%2FRome&${INFO}`);
-        console.log(`${API_URL}matches?date=${formattedDate}&timezone=Europe%2FRome&${INFO}`)
         liveMatches = response.data.leagues;
-        liveMatches.forEach(league => {
-            league.matches.forEach(async match => {
-                const response = await axios.get(`${API_URL}matchDetails?matchId=${match.id}&${INFO}`);
-                matchesDetails.set(match.id, response.data);
+
+        // Usare Promise.all per gestire le richieste asincrone
+        await Promise.all(
+            liveMatches.map(async league => {
+                await Promise.all(
+                    league.matches.map(async match => {
+                        try {
+                            const matchDetailsResponse = await axios.get(`${API_URL}matchDetails?matchId=${match.id}&${INFO}`);
+                            matchesDetails.set(match.id, matchDetailsResponse.data);
+                        } catch (error) {
+                            console.error(`Errore nel recupero dei dettagli per match ID ${match.id}:`, error.message);
+                            // Non lanciare errori qui per non fermare l'esecuzione
+                        }
+                    })
+                );
             })
-        })
-        return liveMatches
+        );
+        return liveMatches;
     } catch (error) {
-        throw new Error(error);
+        console.error('Errore nella funzione uploadLiveMatches:', error.message);
+        // Non lanciare errori qui per non fermare l'esecuzione
+        return [];
     }
 };
 
 const getLiveMatchesSaved = async (matchId) => {
     try {
-        return liveMatches
+        return liveMatches;
     } catch (error) {
-        throw new Error(error);
+        console.error('Errore nella funzione getLiveMatchesSaved:', error.message);
+        return [];  // Assicurati di restituire un valore di fallback
     }
 };
 
 const getFormattedDate = () => {
     const now = Luxon.DateTime.now().setZone('Europe/Rome');
     const formattedDate = now.toFormat('yyyyMMdd');
-    console.log(formattedDate);
     return formattedDate;
-
 };
 
 const debugMatchesDetails = () => {
@@ -56,8 +66,8 @@ const getLiveMatch = async (matchId) => {
     try {
         return matchesDetails.get(Number(matchId));
     } catch (error) {
-        console.error('Error:', error);
-        throw new Error(error);
+        console.error('Errore nella funzione getLiveMatch:', error.message);
+        return null;  // Assicurati di restituire un valore di fallback
     }
 };
 
